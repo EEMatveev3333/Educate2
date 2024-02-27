863ff613859d3fd967bd3643359ae12776ee46cd
EEMatveev <eematveev333@gmail.com>
2024-02-27 23:43:24 +0300

    Commit4

---
+++ src/test/java/org/example/AccountTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void getOwnerName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setOwnerName() {
+    }
+}

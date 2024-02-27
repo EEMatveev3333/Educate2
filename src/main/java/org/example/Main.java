81733978149d7f6333778f1a46e343c90f2cd23b
EEMatveev <eematveev333@gmail.com>
2024-02-27 17:58:02 +0300

    Commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,20 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        Account acc1 = new Account("Ivanov Ivan Ivanovich");
+        System.out.println(acc1);
+        Account acc2 = new Account("Petrov Petr Petrovich");
+        System.out.println(acc2);
+        Account acc3 = new Account("Sidorov Sidor Sidorovich");
+        System.out.println(acc3);
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//        for (int i = 1; i <= 5; i++) {
+//
+//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+//            // for you, but you can always add more by pressing Ctrl+F8.
+//            System.out.println("i = " + i);
+//        }
     }
 }

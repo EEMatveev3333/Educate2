ed316204ad32db05468ec10dd05c294e82356766
EEMatveev <eematveev333@gmail.com>
2024-02-28 15:16:59 +0300

    Commit8

---
+++ src/test/java/org/example/AccountStateSavings.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountStateSavings {
+
+    @Test
+    void saveState() {
+    }
+
+    @Test
+    void loadState() {
+    }
+}
